4d56c7a2bb7d7cd556c5a43a97ac8ad1a85cfbf6
Sally Cai <48254686+scai42@users.noreply.github.com>
2019-09-08 21:31:44 -0700

    initial

---
+++ 100-side-dice.py
@@ -0,0 +1,7 @@
+import random
+import matplotlib
+
+while True:
+    result = random.randint(1,100)
+    print("The dice rolled and you got:  ", result)
+    input("Press any key + RETURN / ENTER to roll again.  ")
